a13799fa2a946d63c323719b43eec3bcfd8e2a4a
David Ockey <David.Ockey@Intel.com>
2015-09-15 17:16:07 -0600

    Added a one second delay after initializing the keyboard, so that the OS would have enough time to recognisethe HID hardware before issuing keystrokes.

--- USB_computer_locker.ino
+++ USB_computer_locker.ino
@@ -1,8 +1,8 @@
-#include <EEPROM.h>
+//#include <EEPROM.h>
 
-#define WINDOWS 0x00
-#define LINUX 0x01
-#define OSX 0x02
+#define WINDOWS 1
+#define LINUX 2
+#define OSX 3
 
 #define LOCKED 1
 #define UNLOCKED 2
@@ -11,23 +11,24 @@
 #define SPEAKER 4
 
 byte lockState;
-byte os;
+int os;
 
 void setup() {
-  EEPROM.get(0x0, os);
-  if (os = 0xFF) {
-    os = 0x00;
-  }
-  Keyboard.begin();
+  //  EEPROM.get(0x0, os);
+  //  if (os = 0xFF) {
+  os = LINUX;
+  //  }
   lockState = 0;
+  Keyboard.begin();
+  delay(1000);
 
-//  pinMode(1, INPUT);
-//  pinMode(2, INPUT);
-//  pinMode(3, INPUT);
+  //  pinMode(1, INPUT);
+  //  pinMode(2, INPUT);
+  //  pinMode(3, INPUT);
 
-//  digitalWrite(1, LOW);
-//  digitalWrite(2, LOW);
-//  digitalWrite(3, LOW);
+  //  digitalWrite(1, LOW);
+  //  digitalWrite(2, LOW);
+  //  digitalWrite(3, LOW);
 
   lock();
 }
@@ -45,13 +46,13 @@ void lock() {
   Keyboard.press(KEY_LEFT_ALT);
   delay(100);
   Keyboard.releaseAll();
-  delay(10);
+  delay(50);
 
   // Regain Host OS focus in Virtual Box
   Keyboard.press(KEY_RIGHT_CTRL);
   delay(100);
   Keyboard.releaseAll();
-  delay(10);
+  delay(50);
 
   switch (os) {
     case WINDOWS:
@@ -69,32 +70,37 @@ void lock() {
       Keyboard.press(KEY_LEFT_SHIFT);
       Keyboard.press(0xB8);
       break;
+    default:
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.press(KEY_LEFT_ALT);
+      Keyboard.press('l');
+      break;
   }
-  delay(100);
+  delay(1000);
   Keyboard.releaseAll();
 }
 
 void loop() {
-//  byte newState;
-//
-//  if (digitalRead(1) == HIGH) {
-//    newState = 1;
-//  } else if (digitalRead(2) == HIGH) {
-//    newState = 2;
-//  } else if (digitalRead(3) == HIGH) {
-//    newState = 3;
-//  }
-//
-//  if (newState != lockState) {
-//    lockState = newState;
-//    switch (lockState) {
-//      case LOCKED:
-//        lock();
-//        break;
-//      case UNLOCKED:
-//        break;
-//      case PANIC:
-//        break;
-//    }
-//  }
+  //  byte newState;
+  //
+  //  if (digitalRead(1) == HIGH) {
+  //    newState = 1;
+  //  } else if (digitalRead(2) == HIGH) {
+  //    newState = 2;
+  //  } else if (digitalRead(3) == HIGH) {
+  //    newState = 3;
+  //  }
+  //
+  //  if (newState != lockState) {
+  //    lockState = newState;
+  //    switch (lockState) {
+  //      case LOCKED:
+  //        lock();
+  //        break;
+  //      case UNLOCKED:
+  //        break;
+  //      case PANIC:
+  //        break;
+  //    }
+  //  }
 }
